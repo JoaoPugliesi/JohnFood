f68f856db26983a7d3396b0ce8c8424c09fcd95e
Joao Pugliesi <joaopaulo.allex@gmail.com>
2023-10-22 19:39:36 -0300

    Subindo aplicação

---
+++ marketplace/src/test/java/io/github/joaopugliesi/johnfood/mp/HelloResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.joaopugliesi.johnfood.mp;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}
